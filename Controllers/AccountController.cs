de75f46886ece0cd72a8091cbccfad2edb116440
Viththaki <vithuviththi123@gmail.com>
2023-07-06 12:50:09 +0530

    added chnages

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -9,12 +9,16 @@ namespace AuthendicationJWTToken.Controllers
     public class AccountController : ControllerBase
     {
         [HttpPost]
+        [Route("Login")]
         public IActionResult Login([FromForm]AuthendicationRequest authendicationRequest)
         {
          //create the object of jwt authendication manager
          var jwtAuthenticationManager = new JwtAuthenticationManager();
          var authResult = jwtAuthenticationManager.Authenticate(authendicationRequest.UserName, authendicationRequest.Password);
-         return Ok(authResult);
+            if (authResult == null)
+                return Unauthorized();
+            else 
+                return Ok(authResult);
         }
     }
 }
