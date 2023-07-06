aba78eb955224697b41c76422d55fdcc16c592b2
Viththaki <vithuviththi123@gmail.com>
2023-07-06 13:41:45 +0530

    remove files

--- Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AuthendicationJWTToken
-{
-    public class Constants
-    {
-        public const int JWT_TOKEN_VALIDITY_MINS = 30;
-        public const string JWT_SECURITY_KEY = "CODING_DROPLETS";
-    }
-}
