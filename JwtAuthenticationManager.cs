b3584c58f5a4ffb3ec60abdf60725039ee7a0b5d
janidulnkg1 <janidulnkg1@gmail.com>
2023-07-06 11:46:05 +0530

    add changes

---
+++ JwtAuthenticationManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthendicationJWTToken
+{
+
+   
+    public class JwtAuthenticationManager
+    {
+       public JwtAuthResponse Authenticate(string userName, string password)
+        {
+            if(userName != "user01" || password != "password123")
+            {
+                return null;
+            }
+
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.ASCII.GetBytes(Constants.JWT_SECURITY_KEY);
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new List<Claim>
+                {
+                    new Claim("username", userName),
+                    new Claim(ClaimTypes.PrimaryGroupSid, "User Group 01")
+                }),
+                Expires = DateTime.Now.AddMinutes(Constants.JWT_TOKEN_VALIDITY_MINS),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+
+        }
+
+
+
+
+
+    }
+}
